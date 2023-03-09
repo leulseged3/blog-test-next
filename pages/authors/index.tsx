bab983365aef556351d731763d933b03127aa298
leulseged tilahun <leulseged3@gmail.com>
2023-03-09 14:30:33 +0300

    authors & edit pages

---
+++ pages/authors/index.tsx
@@ -0,0 +1,8 @@
+
+const Authors = () => {
+  return <div>
+    <h1>authors</h1>
+  </div>
+}
+
+export default Authors
---
+++ pages/blogs/edit.tsx
@@ -0,0 +1,9 @@
+const Edit: React.FC = () => {
+  return (
+    <div>
+      <h2>Edit...</h2>
+    </div>
+  )
+}
+
+export default Edit
